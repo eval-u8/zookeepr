685e067903cd25afe406b56143e6b607ae750447
Ernesto Valecillos <evalecillos@gmail.com>
2021-10-17 18:59:07 -0500

    added validation and post

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
+const fs = require("fs");
+const path = require("path");
 const express = require("express");
 const { animals } = require("./data/animals");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// The express.urlencoded({extended: true}) method is a method built into Express.js. It takes incoming POST data and converts it to key/value pairings that can be accessed in the req.body object. The extended: true option set inside the method call informs our server that there may be sub-array data nested in it as well, so it needs to look as deep into the POST data as possible to parse all of the data correctly.
 
+// parse incoming JSON data
+app.use(express.json());
+// The express.json() method we used takes incoming POST data in the form of JSON and parses it into the req.body JavaScript object. Both of the above middleware functions need to be set up every time you create a server that's looking to accept POST data.
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -53,7 +61,46 @@ function filterByQuery(query, animalsArray) {
 function findById(id, animalsArray) {
     const result = animalsArray.filter((animal) => animal.id === id)[0];
     return result;
-};
+}
+
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+    fs.writeFileSync(
+        path.join(__dirname, "./data/animals.json"),
+        JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+    return animal;
+}
+
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== "string") {
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== "string") {
+        return false;
+    }
+    if (!animal.diet || typeof animal.diet !== "string") {
+        return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false;
+    }
+    return true;
+}
+
+app.post("/api/animals", (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validateAnimal(req.body)) {
+        res.status(400).send("The animal is not properly formatted.");
+    } else {
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
+    }
+});
 
 app.get("/api/animals", (req, res) => {
     let results = animals;
